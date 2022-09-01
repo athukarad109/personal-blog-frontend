7342c4cf218e838d99b8ede3d8e7937be0dc88fd
Atahrv <athukarad109@gmail.com>
2022-09-01 23:26:03 +0530

    Adding add blog

--- src/Components/Fakeadd.js
+++ src/Components/Fakeadd.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Fakeadd = () => {
   return (
     <div>
       <h1>Only I can add here LOL</h1>
+      <Link to="/add/athukarad">
+        <h2 className='footer'>Click</h2>
+      </Link>
     </div>
   )
 }
